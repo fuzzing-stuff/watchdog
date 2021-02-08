f9950ba8aea3cc9562ba5efdf8807e014ead69ab
Sysoev, Vladimir <vladimir.sysoev@sarov-itc.ru>
2021-02-08 19:59:32 +0300

    added version dump

--- main.go
+++ main.go
@@ -8,16 +8,30 @@ import (
 	"time"
 )
 
+var (
+	version string
+)
+
 func ping(c chan bool) func(w http.ResponseWriter, req *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		c <- true
 	}
 }
 
+func printVersionInfo() {
+	fmt.Println("watchdog version in " + version)
+}
+
 func main() {
 	timeout := flag.Duration("timeout", 30*time.Second, "Timeout for ping in sec (default: 30 sec)")
 	url := flag.String("url", ":8090", "URL for check")
+	verInfo := flag.Bool("version", false, "Print version info")
 	flag.Parse()
+
+	if *verInfo {
+		printVersionInfo()
+		return
+	}
 
 	c := make(chan bool)
 
